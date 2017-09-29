faec18eb2041036145e223b3a97db4684a960427
Kasper Declercq <kasper.declercq@somko.be>
2017-09-29 12:34:32 +0200

    add summary

--- renovation_tax_be/__manifest__.py
+++ renovation_tax_be/__manifest__.py
@@ -8,6 +8,7 @@
         Change the VAT tax with one mouse click from 21% to 6%.
         Print VAT certificates
         """,
+    'summary': """Change the VAT tax with one mouse click from 21% to 6%. Print VAT certificates.""",
     'website': 'https://www.somko.be',
     'images': ['static/description/module-cover.png',],
     'depends': ['account', 'sale', 'l10n_be'],
